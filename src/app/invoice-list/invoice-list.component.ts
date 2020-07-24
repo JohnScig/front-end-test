7d1fa992d910eceaeeb4c8e0542c38038d3f2b91
John Scigulinsky <john.scigulinsky@gmail.com>
2020-07-24 10:15:12 +0200

    filtering works

--- src/app/invoice-list/invoice-list.component.ts
+++ src/app/invoice-list/invoice-list.component.ts
@@ -11,10 +11,29 @@ export class InvoiceListComponent implements OnInit {
 
   constructor(private invoiceGeneratorService: InvoiceGeneratorService) { }
 
+  inputSearchElement: HTMLInputElement;
+
+  filterMode = false;
+
   invoices: Invoice[];
+  filteredInvoices: Invoice[];
 
   ngOnInit(): void {
     this.invoiceGeneratorService.getInvoices().subscribe(invoices => this.invoices = invoices);
+    this.inputSearchElement = document.getElementById('search') as HTMLInputElement;
+  }
+
+  filter(event: any) {
+    if (event.target.value === '') {
+      this.filterMode = false;
+    } else {
+      this.filterMode = true;
+    }
+
+    const filterString = (event.target.value);
+
+    this.filteredInvoices = this.invoices.filter(invoice => invoice.name.includes(filterString));
+
   }
 
 }
