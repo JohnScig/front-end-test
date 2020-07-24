d6fe816b5676a22108b675df8bcb472ea79d9053
John Scigulinsky <john.scigulinsky@gmail.com>
2020-07-24 10:56:37 +0200

    list summary works

---
+++ src/app/invoice-list-summary/invoice-list-summary.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Invoice } from '../models/invoice';
+
+@Component({
+  selector: 'app-invoice-list-summary',
+  templateUrl: './invoice-list-summary.component.html',
+  styleUrls: ['./invoice-list-summary.component.scss']
+})
+export class InvoiceListSummaryComponent implements OnInit {
+
+  @Input()
+  set invoices(invoices: Invoice[]) {
+    this.paidCount = invoices.filter(invoice => invoice.price > 0.5 ).length;
+    this.unpaidCount = invoices.filter(invoice => invoice.price <= 0.5 ).length;
+  }
+
+  paidCount: number;
+  unpaidCount: number;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
